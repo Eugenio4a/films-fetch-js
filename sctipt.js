1ae78d25f5d42ca1d6691fc2a4199113df34551a
Eugeniy e-marketing <u1-259@office107.local>
2021-12-23 11:07:32 +0200

    fixing

--- sctipt.js
+++ sctipt.js
@@ -1,54 +1,3 @@
-// searchBtn.addEventListener('click', (e)=> {
-//     let searchValue = search.value;
-//     if (searchValue.length === 0) {
-//         alert('Вы ничего не ввели')
-//         user.innerHTML = '';
-//     }
-//     else
-
-// fetch(url + "/" + `${searchValue}`)
-//     .then(responce => responce.json())
-//     .then(json => json.data)
-//     .then(data => user.innerHTML = `
-
-//         <li><img src="${data.avatar}"</li>
-//         <li><span>First Name:'dsad' </span>${data.first_name}</li>
-//         <li><span>Last Name: </span>${data.last_name}</li>
-//         <li><span>E-mail: </span>${data.email}</li>
-
-//         `)
-//     .catch((error) => {
-//         console.log(error);
-//         if (error.responce === undefined) {
-//             alert("Not Found")
-//         }
-//     });
-// // })
-// email: "janet.weaver@reqres.in", first_name:
-// fetch(url)
-//     .then(responce =>responce.json())
-//     .then(json => json.data)
-//     .then(data => data.forEach((el) => {
-//        console.log(el.email)
-//     }))
-//     .catch((error)=>{
-//         console.error(error);
-//     });
-
-// fetch(url)
-//     .then(request=>request.json())
-//     .then(json=>json.data)
-//     .then(data =>data.forEach((person) => {
-//       user.innerHTML +=
-//       `
-
-//       <ul>
-//       <li>${person.first_name}</li>
-//       <li>${person.email}</li>
-//       <li><img src ="${person.avatar}"></li>
-//       </ul>
-//       `
-//     }))
 const url = "https://reqres.in/api/users";
 const search = document.querySelector("#search");
 const searchBtn = document.querySelector("#search_btn");
@@ -64,7 +13,7 @@ searchBtn.addEventListener("click", (e) => {
     user.slyle.display = "none";
   } else search.classList.remove("nothing_write");
 
-  fetch(`http://www.omdbapi.com/?t=${search.value}&apikey=${api}`)
+  fetch(`https://www.omdbapi.com/?t=${search.value}&apikey=${api}`)
     .then((response) => response.json())
     .then((movie) => {
       if (movie.Poster == undefined && searchValue.length !== 0) {
@@ -88,7 +37,7 @@ searchBtn.addEventListener("click", (e) => {
             `;
       user.className = "result";
     });
-  fetch(`http://www.omdbapi.com/?t=${search.value}&apikey=${api}`)
+  fetch(`https://www.omdbapi.com/?t=${search.value}&apikey=${api}`)
     .then((response) => response.json())
     .then((json) => json.Ratings)
     .then((rating) =>
@@ -108,7 +57,7 @@ searchBtn.addEventListener("click", (e) => {
 });
 
 newFilms.addEventListener("click", function () {
-  fetch(`http://www.themoviedb.org/terms-of-use`)
+  fetch(`https://www.themoviedb.org/terms-of-use`)
     .then((response) => response.json())
     .then((mov) => console.log(mov));
 });
